03fae98ae242a9f5e9e133a3ad6660ef1d4e244c
Sebastian Revuelta <sebastianrevuelta@gmail.com>
2024-06-11 10:56:54 +0200

    Update IPAddressExample.cs

--- IPAddressExample.cs
+++ IPAddressExample.cs
@@ -8,7 +8,7 @@ public class IPAddressExample
 
     public IPAddressExample()
     {
-        string myIP = "123.168.97.54";
+        string myIP = "123.168.97.53";
 
         //ok: avoid_ip_address_in_the_code
         hardcodedIpAddress = IPAddress.Parse("192.168.4.1"); 

3aa9b810d047c0ab28757ce8374d05ae0a8dbb5e
idan123 <idanc@getvim.com>
2024-06-13 11:04:20 +0000

    test01

---
+++ .circleci/base_config/semgrep.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo 'export SEMGREP_APP_TOKEN=$SEMGREP_APP_TOKEN' >> $BASH_ENV
+echo 'export SEMGREP_COMMIT=$CIRCLE_SHA1' >> $BASH_ENV
+echo 'export SEMGREP_JOB_URL=$CIRCLE_BUILD_URL' >> $BASH_ENV
+PR_NUMBER=$(echo "$CIRCLE_PULL_REQUEST" | awk -F '/' '{print $NF}' )
+if [ -n "$PR_NUMBER" ]; then 
+    echo 'export SEMGREP_BASELINE_REF = "origin/<< pipeline.parameters.master_branch >>"' >> $BASH_ENV
+    echo "Pull Request Number: $PR_NUMBER"
+    echo 'export SEMGREP_PR_ID=$PR_NUMBER' >> $BASH_ENV
+    git fetch origin "+refs/heads/*:refs/remotes/origin/*"
+    semgrep ci --baseline-commit=$(git merge-base development HEAD)
+else
+    if [ "$CIRCLE_BRANCH" == "development" ]; then
+        echo "Running Full scan for branch: $CIRCLE_BRANCH"
+        semgrep ci || true
+    else
+        echo "Skipping full scan for branches different to development"
+    fi
+fi

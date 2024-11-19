7d5328758204d2f50c9ded9502d363a839e10b44
sebasrevuelta <122784773+sebasrevuelta@users.noreply.github.com>
2024-11-19 17:04:54 +0100

    Update semgrep.sh

--- .circleci/base_config/semgrep.sh
+++ .circleci/base_config/semgrep.sh
@@ -54,7 +54,7 @@ if [ -n "$PR_NUMBER" ]; then
                 common_directory=$(dirname "$common_directory")
             done
             echo "Directory to scan: $common_directory"
-            semgrep ci --baseline-commit=$(git merge-base development HEAD) --max-memory 3700 -j 5 --subdir=$common_directory || true
+            semgrep ci --baseline-commit=$(git merge-base development HEAD) --max-memory 3700 -j 5 --subdir=A || true
         fi
     fi
 fi
